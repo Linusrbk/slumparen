70c1c7a4537179d7c3d46a779741acde1517551f
Linusrbk <linus.astrom@elev.ga.ntig.se>
2021-02-01 13:40:18 +0100

    the layers of hell from dantes inferno and heaven became a part of hell

---
+++ layers of hell.py
@@ -0,0 +1,16 @@
+import random
+loh=[
+    'First layer: Limbo',
+    'Second layer:Lust',
+    'Third layer:Gluttony',
+    'Fourth layer:Greed',
+    'Fifth layer:Anger',
+    'Sixth layer:Heresy',
+    'Seventh layer:Violence',
+    'Eighth layer:Fraud',
+    'Ninth layer:Treachery',
+    'Tenth layer:Heaven'#Heaven is a part of hell cause we can't have nice things.
+]
+loha=random.randint(0,len(loh)-1)
+
+print(f"{loh[loha]}")
